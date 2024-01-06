62cc9abd0039efd8286da779d9f0b352a32a00db
Bogomazov Andrei <swchck@gmail.com>
2024-01-06 17:04:21 +0000

    Update dotfiles

--- install_macos.sh
+++ install_macos.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
+# On MacOS should be run with sudo privileges
+
 set -e
 
 GITHUB_USER=swchck
 
 if [ ! "$(command -v chezmoi)" ]; then
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot "$GITHUB_USER"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b $HOME/.local/bin
+    sh -c "$(wget -qO- get.chezmoi.io)" -- init --one-shot "$GITHUB_USER"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
 fi
-
-export PATH="$HOME/.local/bin:$PATH"
-exec chezmoi init --apply "$GITHUB_USER"
