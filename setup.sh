e5fda98da3611ef162f3a8419639a59006e8e59d
Bogomazov Andrei <swchck@gmail.com>
2024-12-24 23:25:24 +0000

    update installation script and add devcontainer

--- setup.sh
+++ setup.sh
@@ -1,20 +1,70 @@
 #!/usr/bin/env bash
 
+GITHUB_USER="swchck"
+
 # xcode_select_setup function.
 # This function installs XCode Command Line Tools and waits until installation has finished.
 function xcode_select_setup() {
+  # Check if XCode Command Line Tools are already installed.
+  if xcode-select --print-path >/dev/null 2>&1; then
+    echo "XCode Command Line Tools are already installed."
+    return
+  fi
+
   # Install XCode Command Line Tools.
-  xcode-select --install > /dev/null 2>&1
+  xcode-select --install >/dev/null 2>&1
 
   # Wait until XCode Command Line Tools installation has finished.
   # This is necessary because the XCode CLI installation is a GUI process.
-  until xcode-select --print-path > /dev/null 2>&1; do
-    sleep 5;
+  until xcode-select --print-path >/dev/null 2>&1; do
+    sleep 5
   done
 }
 
+# chezmoi_install function.
+# This function installs Chezmoi and calls Chezmoi One-Shot Install.
+function chezmoi_install() {
+  set -e
+
+  local IS_INSTALLED=0
+  local USE_INSTALLED=0
+
+  # Check if Chezmoi is already installed.
+  if [ "$(command -v chezmoi)" ]; then
+    IS_INSTALLED=1
+  fi
+
+  # Prompt for confirmation to install Chezmoi.
+  if [ "$IS_INSTALLED" -eq 1 ]; then
+    read -pr "Chezmoi is already installed on your system. Do you want to use installed version? (y/n): " CONFIRM
+    if [ "$CONFIRM" == "y" ]; then
+      USE_INSTALLED=1
+    fi
+  else
+    echo "Chezmoi will be downloaded and installed."
+
+    # Download Chezmoi binary to .local/bin directory.
+    echo "Download latest version of Chezmoi from chezmoi.io..."
+    if [ "$(command -v curl)" ]; then
+      curl -sfL https://get.chezmoi.io/lb | sh
+    elif [ "$(command -v wget)" ]; then
+      wget -qO- https://get.chezmoi.io/lb | sh
+    else
+      echo "curl or wget is required to download Chezmoi binary." >&2
+      exit 1
+    fi
+  fi
+
+  # Run Chezmoi One-Shot Install.
+  if [ "$USE_INSTALLED" -eq 1 ]; then
+    chezmoi apply --one-shot --verbose
+  else
+    .local/bin/chezmoi init --apply "$GITHUB_USER"
+  fi
+}
+
 # macOS_setup function.
-# This function installs XCode Command Line Tools, 
+# This function installs XCode Command Line Tools,
 # waits until installation has finished, and calls Chezmoi One-Shot Install.
 # Arguments:
 #   $1: GitHub username.
@@ -23,27 +73,24 @@ function macOS_setup() {
   xcode_select_setup
 
   set -e
-  
-  # Call Chezmoi One-Shot Install.
-  if [ ! "$(command -v chezmoi)" ]; then
-    if [ "$(command -v curl)" ]; then
-      sh -c "$(curl -fsLS get.chezmoi.io)" -- init --one-shot "$1"
-    elif [ "$(command -v wget)" ]; then
-      sh -c "$(wget -qO- get.chezmoi.io)" -- init --one-shot "$1"
-    else
-      echo "To install chezmoi, you must have curl or wget installed." >&2
-      exit 1
-    fi
-  else
-    # If Chezmoi is already installed, prompt for confirmation to run Chezmoi One-Shot Install.
-    read -p "Chezmoi is already installed. Do you want to run Chezmoi One-Shot Install? (y/n): " CONFIRM
-    if [ "$CONFIRM" = "y" ]; then
-      chezmoi init --one-shot "$1"
-    else
-      echo "Chezmoi One-Shot Install was not run." >&2
-    fi
+
+  # Install and run Chezmoi One-Shot Install.
+  chezmoi_install
+}
+
+# cleanup function.
+# This function removes Chezmoi binary from .local/bin directory.
+function cleanup() {
+  # Check if Chezmoi binary is in .local/bin directory.
+  if [ -f ~/.local/bin/chezmoi ]; then
+    # Remove Chezmoi binary.
+    echo "Remove downloaded binary from .local/bin directory."
+    rm .local/bin/chezmoi
   fi
 }
+
+# Register trap on any signal to call cleanup function.
+trap cleanup EXIT
 
 # Check OS (macOS only).
 # TODO: Add support for Linux.
@@ -52,8 +99,5 @@ if [ "$(uname)" != "Darwin" ]; then
   exit 1
 fi
 
-# Prompt for GitHub username.
-read -p "Enter GitHub username: " GITHUB_USER
-
 # Call macOS_setup function with GitHub username as argument.
-macOS_setup "$GITHUB_USER"
+macOS_setup
